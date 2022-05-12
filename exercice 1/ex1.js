be40bfc9c13e4ae7164284888d945a3d1a5f33c0
racha <rachabnabid@gmail.com>
2022-05-12 15:46:06 +0100

    r

--- exercice 1/ex1.js
+++ exercice 1/ex1.js
@@ -1,13 +1,14 @@
 
   var min1 = Math.min(100, 2, 25, 36, 57,3);
   console.log(min1);
+  var tab = [100, 2, 25, 36, 57,3];
    for (let i = 0; i<tab.length; i++) {
-   if (tab[i] == min1) {
-    console.log(tab.slice(i,1));
+   result = tab.filter(element => {return element.tab[i] == min1})
+   console.log(result);
   }
-   }
-   var tab = [100, 2, 25, 36, 57,3];
-   const result = tabs.filter(tab => Math.min(tab));
+   
+   
+   
 
    
    
