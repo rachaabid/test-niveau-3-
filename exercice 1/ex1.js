e722a90db89aa24abd31975b5868592ea9372734
racha <rachabnabid@gmail.com>
2022-05-12 15:39:09 +0100

    ex1

---
+++ exercice 1/ex1.js
@@ -0,0 +1,13 @@
+
+  var min1 = Math.min(100, 2, 25, 36, 57,3);
+  console.log(min1);
+   for (let i = 0; i<tab.length; i++) {
+   if (tab[i] == min1) {
+    console.log(tab.slice(i,1));
+  }
+   }
+   var tab = [100, 2, 25, 36, 57,3];
+   const result = tabs.filter(tab => Math.min(tab));
+
+   
+   
